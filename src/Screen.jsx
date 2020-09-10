0827ab9cd0619890f7060f461d2cff3e0a968dca
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-09-10 13:38:44 -0700

    Implementing html css

--- src/Screen.jsx
+++ src/Screen.jsx
@@ -2,18 +2,24 @@ import React from "react";
 
 function Screen() {
     return (
-        <div>
-            <div className="screen">
-                <h1>This is a screen!</h1>
-            </div>
-
-            <div className="btn">
-                <h1>=</h1>
-            </div>
-
-            <div className="btn">
-                <h1>+</h1>
-            </div>
+        <div className="parent">
+            <div className="div1">+</div>
+            <div className="div2">-</div>
+            <div className="div3">/</div>
+            <div className="div4">*</div>
+            <div className="div5">7</div>
+            <div className="div6">8</div>
+            <div className="div7">9</div>
+            <div className="div8">4</div>
+            <div className="div9">5</div>
+            <div className="div10">6</div>
+            <div className="div11">1</div>
+            <div className="div12">2</div>
+            <div className="div13">3</div>
+            <div className="div14">0</div>
+            <div className="div15">.</div>
+            <div className="div16">AC</div>
+            <div className="div17">=</div>
         </div>
     );
 }
