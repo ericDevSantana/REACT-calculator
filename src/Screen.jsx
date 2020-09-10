00dfdf37c151d38cedbd6b51585388edfb5eac56
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-09-10 07:26:01 -0700

    First Commit

---
+++ src/Screen.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function Screen() {
+    return (
+        <div>
+            <div className="screen">
+                <h1>This is a screen!</h1>
+            </div>
+
+            <div className="btn">
+                <h1>=</h1>
+            </div>
+
+            <div className="btn">
+                <h1>+</h1>
+            </div>
+        </div>
+    );
+}
+
+export default Screen;
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Screen from "./Screen";
+
+ReactDOM.render(
+  <Screen />,
+  document.getElementById('root')
+);
