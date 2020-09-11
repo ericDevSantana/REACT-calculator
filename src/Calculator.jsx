e3315f7c42f4610b165a7d9bc8ad48d4ad1f9852
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-09-11 15:32:48 -0700

    adding first number

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,11 +1,33 @@
 import React from "react";
 
 function Calculator() {
-   
+   const [currentNumber, setCurrentNumber] = React.useState("0");
+
+   function setNumber(number){
+       let aux = "";
+
+       if(number !== "AC"){
+           if(currentNumber === "0" && number.toString() === "0"){
+               aux = "0";
+           } else if(currentNumber === "0" && number.toString() !== "0"){
+               setCurrentNumber("");
+               aux = number.toString();
+           } else {
+               aux = currentNumber+number;
+           }
+           setCurrentNumber(aux);
+       } else {
+           setCurrentNumber("0");
+       }
+
+   }
+
     return (
         <div className="parent">
-            <div className="screen">123456</div>
-            <div id="operator" className="operator1">
+            <div className="screen">
+                <h3>{currentNumber}</h3>
+            </div>
+            <div id="operator" className="operator1" onClick={() => {console.log("+")}}>
                 <h3>+</h3>
             </div>
             <div id="operator" className="operator2">
@@ -18,18 +40,18 @@ function Calculator() {
                 <h3>*</h3>
             </div>
             <div id="equal" className="operator-equal">=</div>
-            <div id="numbers" className="digit7">7</div>
-            <div id="numbers" className="digit8">8</div>
-            <div id="numbers" className="digit9">9</div>
-            <div id="numbers" className="digit4">4</div>
-            <div id="numbers" className="digit5">5</div>
-            <div id="numbers" className="digit6">6</div>
-            <div id="numbers" className="digit1">1</div>
-            <div id="numbers" className="digit2">2</div>
-            <div id="numbers" className="digit3">3</div>
+            <div id="numbers" className="digit7" onClick={() => {setNumber(7)}}>7</div>
+            <div id="numbers" className="digit8" onClick={() => {setNumber(8)}}>8</div>
+            <div id="numbers" className="digit9" onClick={() => {setNumber(9)}}>9</div>
+            <div id="numbers" className="digit4" onClick={() => {setNumber(4)}}>4</div>
+            <div id="numbers" className="digit5" onClick={() => {setNumber(5)}}>5</div>
+            <div id="numbers" className="digit6" onClick={() => {setNumber(6)}}>6</div>
+            <div id="numbers" className="digit1" onClick={() => {setNumber(1)}}>1</div>
+            <div id="numbers" className="digit2" onClick={() => {setNumber(2)}}>2</div>
+            <div id="numbers" className="digit3" onClick={() => {setNumber(3)}}>3</div>
+            <div id="numbers" className="digit0" onClick={() => {setNumber(0)}}>0</div>
             <div id="numbers" className="digitDot">.</div>
-            <div id="numbers" className="digit0">0</div>
-            <div id="numbers" className="digitAC">AC</div>
+            <div id="numbers" className="digitAC" onClick={() => {setNumber("AC")}}>AC</div>
         </div>
     );
 }
