2f872c2d58f1a105bd0a2feeeaae1ff4bd3a576a
Eric <22939634+ericDevSantana@users.noreply.github.com>
2020-09-12 00:48:55 -0700

    calculator states

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -4,57 +4,74 @@ function Calculator() {
    const [currentNumber, setCurrentNumber] = React.useState("0");
    const [lastNumber, setLastNumber] = React.useState("");
    const [isFirst, setBool] = React.useState(true);
-   let operator = "";
+   const [operator, setOperator] = React.useState("");
+   const [onResult, setOnResult] = React.useState(false);
 
    function setNumber(number){
        let aux = "";
 
-       if(isFirst){
+       if(!onResult){
+            if(isFirst){
+                    if(number !== "AC"){
+                        if(currentNumber === "0" && number.toString() === "0"){
+                            aux = "0";
+                        } else if(currentNumber === "0" && number.toString() !== "0"){
+                            setCurrentNumber("");
+                            aux = number.toString();
+                        } else {
+                            aux = currentNumber+number;
+                        }
+                        setCurrentNumber(aux);
+                    } else {
+                        setCurrentNumber("0");
+                    }
+            } else {
+                    if(number !== "AC"){
+                        if(lastNumber === "0" && number.toString() === "0"){
+                            aux = "0";
+                        } else if(lastNumber === "0" && number.toString() !== "0"){
+                            setLastNumber("");
+                            aux = number.toString();
+                        } else {
+                            aux = lastNumber+number;
+                        }
+                        setLastNumber(aux);
+                    } else {
+                        setLastNumber("0");
+                    }
+            }
+        } else {
             if(number !== "AC"){
-                if(currentNumber === "0" && number.toString() === "0"){
-                    aux = "0";
-                } else if(currentNumber === "0" && number.toString() !== "0"){
-                    setCurrentNumber("");
-                    aux = number.toString();
-                } else {
-                    aux = currentNumber+number;
-                }
-                setCurrentNumber(aux);
+                setCurrentNumber(number);
             } else {
                 setCurrentNumber("0");
             }
-       } else {
-            if(number !== "AC"){
-                if(lastNumber === "0" && number.toString() === "0"){
-                    aux = "0";
-                } else if(lastNumber === "0" && number.toString() !== "0"){
-                    setLastNumber("");
-                    aux = number.toString();
-                } else {
-                    aux = lastNumber+number;
-                }
-                setLastNumber(aux);
-            } else {
-                setLastNumber("0");
-            }
-       }
+            setLastNumber("");
+            setOperator("");
+            setOnResult(false);
+            setBool(true);
+        }
 
    }
 
    function addNumber() {
        setLastNumber(Number(currentNumber) + Number(lastNumber));
+       setOnResult(true);
    }
 
    function subtractNumber() {
-       return Number(currentNumber) - Number(lastNumber);
+       setLastNumber(Number(currentNumber) - Number(lastNumber));
+       setOnResult(true);
    }
 
    function divideNumber() {
-       return Number(currentNumber) / Number(lastNumber);
+       setLastNumber(Number(currentNumber) / Number(lastNumber));
+       setOnResult(true);
    }
 
    function multiplyNumber() {
-       return Number(currentNumber) * Number(lastNumber);
+       setLastNumber(Number(currentNumber) * Number(lastNumber));
+       setOnResult(true);
    }
 
     return (
@@ -62,19 +79,29 @@ function Calculator() {
             <div className="screen">
                 <h3>{isFirst ? currentNumber : lastNumber}</h3>
             </div>
-            <div id="operator" className="operator1" onClick={() => {setBool(false); operator = "+"; console.log(isFirst)}}>
+            <div id="operator" className="operator1" onClick={() => {setBool(false); setOperator("+")}}>
                 <h3>+</h3>
             </div>
-            <div id="operator" className="operator2">
+            <div id="operator" className="operator2" onClick={() => {setBool(false); setOperator("-")}}>
                 <h3>-</h3>
             </div>
-            <div id="operator" className="operator3">
+            <div id="operator" className="operator3" onClick={() => {setBool(false); setOperator("/")}}>
                 <h3>/</h3>
             </div>
-            <div id="operator" className="operator4">
+            <div id="operator" className="operator4" onClick={() => {setBool(false); setOperator("*")}}>
                 <h3>*</h3>
             </div>
-            <div id="equal" className="operator-equal" onClick={() => {addNumber()}}>=</div>
+            <div id="equal" className="operator-equal" onClick={() => {
+                if(operator === "+"){
+                    addNumber();
+                } else if(operator === "-") {
+                    subtractNumber();
+                } else if(operator === "/") {
+                    divideNumber();
+                } else if(operator === "*") {
+                    multiplyNumber();
+                }
+            }}>=</div>
             <div id="numbers" className="digit7 digits" onClick={() => {setNumber(7)}}>7</div>
             <div id="numbers" className="digit8 digits" onClick={() => {setNumber(8)}}>8</div>
             <div id="numbers" className="digit9 digits" onClick={() => {setNumber(9)}}>9</div>
